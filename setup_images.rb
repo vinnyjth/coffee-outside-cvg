9bc81ac9eaf4619fa9e77bf430b3db47f024bcfc
Vincent Wilson <vince@classyh.at>
2020-08-25 08:40:10 -0400

    Page speed improvements

--- setup_images.rb
+++ setup_images.rb
@@ -6,11 +6,23 @@ Dir.glob("assets/raw_images/*").each do |i|
   image.resize "400x400^"
   image.gravity 'center'
   image.crop '400x400+0+0'
+  image.quality '85'
+  image.interlace 'JPEG'
+  image.strip
+  image.interlace 'plane'
+  image.colorspace 'RGB'
+  image.sampling_factor '4:2:0'
   image.write "assets/thumbnails/#{i.split('/')[-1]}"
 end
 
 Dir.glob("assets/raw_images/*").each do |i|
   image = MiniMagick::Image.open("./#{i}")
   image.resize "2000x2000>"
+  image.quality '85'
+  image.interlace 'JPEG'
+  image.strip
+  image.interlace 'plane'
+  image.colorspace 'RGB'
+  image.sampling_factor '4:2:0'  
   image.write "assets/full_size/#{i.split('/')[-1]}"
 end
